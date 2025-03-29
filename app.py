479a2d8cc4084640264d4371d97f9e4ecf8c990f
micrighost <45351256+micrighost@users.noreply.github.com>
2025-03-29 22:46:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -177,32 +177,32 @@ def handle_message(event):
             # 驗證是否有辦法抓到資料
             if WebCrawler_MIS_TWSE.webcrawler_ture(text):
                 
-                # 載入快速選單圖片
-                details_icon = request.url_root + 'static/自以為是的佩佩切出.png'
-                details_icon = details_icon.replace("http", "https")
+                # # 載入快速選單圖片
+                # details_icon = request.url_root + 'static/自以為是的佩佩切出.png'
+                # details_icon = details_icon.replace("http", "https")
 
 
-                current_transaction_price_icon = request.url_root + 'static/快樂的佩佩.png'
-                current_transaction_price_icon  = current_transaction_price_icon.replace("http", "https")
+                # current_transaction_price_icon = request.url_root + 'static/快樂的佩佩.png'
+                # current_transaction_price_icon  = current_transaction_price_icon.replace("http", "https")
 
-                best_five_tick_icon = request.url_root + 'static/悲傷的佩佩.png'
-                best_five_tick_icon  = best_five_tick_icon.replace("http", "https")
+                # best_five_tick_icon = request.url_root + 'static/悲傷的佩佩.png'
+                # best_five_tick_icon  = best_five_tick_icon.replace("http", "https")
 
 
-                datetime_icon = request.url_root + 'static/calendar.png'
-                datetime_icon = datetime_icon.replace("http", "https")
-                date_icon = request.url_root + 'static/calendar.png'
-                date_icon = date_icon.replace("http", "https")
-                time_icon = request.url_root + 'static/time.png'
-                time_icon = time_icon.replace("http", "https")
+                # datetime_icon = request.url_root + 'static/calendar.png'
+                # datetime_icon = datetime_icon.replace("http", "https")
+                # date_icon = request.url_root + 'static/calendar.png'
+                # date_icon = date_icon.replace("http", "https")
+                # time_icon = request.url_root + 'static/time.png'
+                # time_icon = time_icon.replace("http", "https")
 
                 
-                # details_icon = "https://susunoyes.onrender.com/static/%E8%87%AA%E4%BB%A5%E7%82%BA%E6%98%AF%E7%9A%84%E4%BD%A9%E4%BD%A9%E5%88%87%E5%87%BA.png"
-                # current_transaction_price_icon = "https://susunoyes.onrender.com/static/%E8%87%AA%E4%BB%A5%E7%82%BA%E6%98%AF%E7%9A%84%E4%BD%A9%E4%BD%A9%E5%88%87%E5%87%BA.png"
-                # best_five_tick_icon  = "https://susunoyes.onrender.com/static/%E8%87%AA%E4%BB%A5%E7%82%BA%E6%98%AF%E7%9A%84%E4%BD%A9%E4%BD%A9%E5%88%87%E5%87%BA.png"
-                # datetime_icon = "https://susunoyes.onrender.com/static/%E8%87%AA%E4%BB%A5%E7%82%BA%E6%98%AF%E7%9A%84%E4%BD%A9%E4%BD%A9%E5%88%87%E5%87%BA.png"
-                # date_icon = "https://susunoyes.onrender.com/static/%E8%87%AA%E4%BB%A5%E7%82%BA%E6%98%AF%E7%9A%84%E4%BD%A9%E4%BD%A9%E5%88%87%E5%87%BA.png"
-                # time_icon = "https://susunoyes.onrender.com/static/%E8%87%AA%E4%BB%A5%E7%82%BA%E6%98%AF%E7%9A%84%E4%BD%A9%E4%BD%A9%E5%88%87%E5%87%BA.png"
+                details_icon = "https://susunoyes.onrender.com/static/%E8%87%AA%E4%BB%A5%E7%82%BA%E6%98%AF%E7%9A%84%E4%BD%A9%E4%BD%A9%E5%88%87%E5%87%BA.png"
+                current_transaction_price_icon = "https://susunoyes.onrender.com/static/%E8%87%AA%E4%BB%A5%E7%82%BA%E6%98%AF%E7%9A%84%E4%BD%A9%E4%BD%A9%E5%88%87%E5%87%BA.png"
+                best_five_tick_icon  = "https://susunoyes.onrender.com/static/%E8%87%AA%E4%BB%A5%E7%82%BA%E6%98%AF%E7%9A%84%E4%BD%A9%E4%BD%A9%E5%88%87%E5%87%BA.png"
+                datetime_icon = "https://susunoyes.onrender.com/static/%E8%87%AA%E4%BB%A5%E7%82%BA%E6%98%AF%E7%9A%84%E4%BD%A9%E4%BD%A9%E5%88%87%E5%87%BA.png"
+                date_icon = "https://susunoyes.onrender.com/static/%E8%87%AA%E4%BB%A5%E7%82%BA%E6%98%AF%E7%9A%84%E4%BD%A9%E4%BD%A9%E5%88%87%E5%87%BA.png"
+                time_icon = "https://susunoyes.onrender.com/static/%E8%87%AA%E4%BB%A5%E7%82%BA%E6%98%AF%E7%9A%84%E4%BD%A9%E4%BD%A9%E5%88%87%E5%87%BA.png"
                 print(details_icon)
 
                 
